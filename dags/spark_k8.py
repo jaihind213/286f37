ff8a694c76e0bc8532c02821cc5582e31891749c
vishnu rao <jaihind213@gmail.com>
2025-06-29 18:06:55 +0800

    Update spark_k8.py

--- dags/spark_k8.py
+++ dags/spark_k8.py
@@ -151,7 +151,6 @@ with DAG(
     schedule_interval="@daily",
     start_date=datetime.now(),
     catchup=False,
-    retries=1,
     template_searchpath=["/tmp/dag_templates"],
     params={
         "date": Param("2024-04-20", type="string"),
